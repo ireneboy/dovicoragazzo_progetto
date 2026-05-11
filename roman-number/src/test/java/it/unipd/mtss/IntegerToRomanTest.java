23e5e3b742c29da33fe592a7653131ce108b416c
aurora spupets <auroraspupets@MacBook-Air-di-aurora.local>
2026-05-11 17:22:11 +0200

    Implementata conversione e stampa numeri 1-20 con relativi test #ID

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -11,58 +11,33 @@ import org.junit.Test;
 public class IntegerToRomanTest {
 
     @Test
-    public void testConversioneNumeriUnoATre() {
+    public void testConversioneUnoADieci() {
         assertEquals("I", IntegerToRoman.convert(1));
-        assertEquals("II", IntegerToRoman.convert(2));
-        assertEquals("III", IntegerToRoman.convert(3));
-    }
-
-    @Test
-    public void testConversioneNumeriQuattroASei() {
         assertEquals("IV", IntegerToRoman.convert(4));
         assertEquals("V", IntegerToRoman.convert(5));
-        assertEquals("VI", IntegerToRoman.convert(6));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testNumeroMinoreDiUno() {
-        IntegerToRoman.convert(0);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testNumeroMaggioreDiSei() {
-        IntegerToRoman.convert(7);
-    }
-
-    @Test
-    public void testConversioneNumero7() {
         assertEquals("VII", IntegerToRoman.convert(7));
-    }
-
-    @Test
-    public void testConversioneNumero8() {
-        assertEquals("VIII", IntegerToRoman.convert(8));
-    }
-
-    @Test
-    public void testConversioneNumero9() {
         assertEquals("IX", IntegerToRoman.convert(9));
-    }
-
-    @Test
-    public void testConversioneNumero10() {
         assertEquals("X", IntegerToRoman.convert(10));
     }
 
-    // --- TEST DEI CASI LIMITE AGGIORNATI ---
+    @Test
+    public void testConversioneUndiciAVenti() {
+        assertEquals("XI", IntegerToRoman.convert(11));
+        assertEquals("XIV", IntegerToRoman.convert(14));
+        assertEquals("XV", IntegerToRoman.convert(15));
+        assertEquals("XVIII", IntegerToRoman.convert(18));
+        assertEquals("XIX", IntegerToRoman.convert(19));
+        assertEquals("XX", IntegerToRoman.convert(20));
+    }
+
     @Test(expected = IllegalArgumentException.class)
-    public void testNumeroMinoreDiUno() {
+    public void testNumeroSottoIlRange() {
         IntegerToRoman.convert(0);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testNumeroMaggioreDiDieci() {
-        // Ora testiamo l'11, perché fino a 10 è tutto valido!
-        IntegerToRoman.convert(11);
+    public void testNumeroSopraIlRange() {
+        // Ora il limite è 20, quindi il primo numero che deve dare errore è 21
+        IntegerToRoman.convert(21);
     }
 }
